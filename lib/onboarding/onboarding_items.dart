4651fa65439f670674b6a1f9f99ec8faa8297d70
Dilshan <ndkumarawansha@gmail.com>
2024-05-10 13:53:43 +0530

    One time Onboarding screen

---
+++ lib/onboarding/onboarding_items.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_app/onboarding/onboarding_info.dart';
+
+class OnBoardingItems {
+  List<OnBoardingInfo> items = [
+    OnBoardingInfo(
+        title: "Laboratory",
+        description:
+            "A scientist in his laboratory is not a mere technician: he is also a child confronting natural phenomena that impress him as though they were fairy tales.",
+        image: "assets/onboarding1.gif"),
+    OnBoardingInfo(
+        title: "Diagnosis",
+        description:
+            "There is little you can do to stop a tornado, a hurricane, or a cancer diagnosis from changing your life in an instant.",
+        image: "assets/onboarding2.gif"),
+    OnBoardingInfo(
+        title: "Chronic Diseases",
+        description:
+            "Cancer taught my family that my mom is much stronger than we ever thought.",
+        image: "assets/onboarding3.gif"),
+    OnBoardingInfo(
+        title: "Heart Diseases",
+        description:
+            "A healthy heart is a key to happiness in life so put a stop to all of the problems related to your heart before it stops you.",
+        image: "assets/onboarding4.gif"),
+  ];
+}

82113fcc46fe9ee3e69ee3a2e4785a877110e707
Dilshan <ndkumarawansha@gmail.com>
2024-05-23 20:26:51 +0530

    OTP Screen page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/Auth/otp_screen.dart';
 import 'package:flutter_app/pages/Auth/register.dart';
 // import 'package:shared_preferences/shared_preferences.dart';
 
@@ -8,20 +9,36 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () async {
-            // final press = await SharedPreferences.getInstance();
-            // press.setBool("onBoarding", false);
-
-             Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const Register(),
-                ));
-          },
-          child: const Text("Register Page"),
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            onPressed: () async {
+              // final press = await SharedPreferences.getInstance();
+              // press.setBool("onBoarding", false);
+          
+               Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Register(),
+                  ));
+            },
+            child: const Text("Register Page"),
+          ),
+      
+          TextButton(
+            onPressed: () async {
+          
+               Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const OTPScreen(),
+                  ));
+            },
+            child: const Text("OTP Screen"),
+          ),
+        ],
       ),
     );
   }

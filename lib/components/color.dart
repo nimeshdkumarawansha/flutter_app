8ceec54c453a0dcbe97b9ad846e29f67771b79f8
Dilshan <ndkumarawansha@gmail.com>
2024-11-25 19:24:32 +0530

    color changes

--- lib/components/color.dart
+++ lib/components/color.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF7357a4);
 const kGrayColor = Color.fromARGB(255, 34, 34, 34);
-const kGreenColor = Color.fromARGB(255, 6, 35, 9);
+const kGreenColor = Color.fromARGB(255, 34, 98, 40);
